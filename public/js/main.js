0ddb0e257b2399000e47be62588b1d1a423ab02b
sempjc <jc.semprit@hotmail.com>
2015-08-31 15:31:44 -0400

    Rewrite the portfolio, now include parallax FX

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
   $(".button-collapse").sideNav({closeOnClick: true});
   $('.scrollspy').scrollSpy();
   $('.slider').slider({full_width: true});
-
+  $('.parallax').parallax();
 });
