64e00a0a26f379985fca88b89f0e968b581769bb
sempjc <jc.semprit@hotmail.com>
2015-09-08 19:35:17 -0400

    Add comment and the fuction section_indicator and the click event

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
+//indicator for current section position on sidenav
+var section_indicator = function(){
+  $(this).siblings().removeClass('active-list');
+  $(this).addClass('active-list');
+}
 $(document).ready(function(){
+  //Initialization of materialize css component
   $(".button-collapse").sideNav({closeOnClick: true});
   $('.scrollspy').scrollSpy();
   $('.slider').slider({full_width: true});
   $('.parallax').parallax();
+
+  //My custom jQuery
+  $('.list-item').click(section_indicator);
+
 });
